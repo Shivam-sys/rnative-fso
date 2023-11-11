6f23254b92972260412b3604343e4c4d8b9cd668
Shivam Kumar <txt.shivam@gmail.com>
2023-11-11 13:31:25 +0530

    Get data from backend

---
+++ src/components/useRepositories.js
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+
+const useRepositories = () => {
+  const [repositories, setRepositories] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const fetchRepositories = async () => {
+    setLoading(true);
+
+    const response = await fetch("http://172.20.10.2:5001/api/repositories");
+    const json = await response.json();
+
+    setLoading(false);
+    setRepositories(json);
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  return { repositories, loading, refetch: fetchRepositories };
+};
+
+export default useRepositories;
+
+// Example repository can be used for demo if backend fails. -
+// const repositories = [
+//   {
+//     id: "jaredpalmer.formik",
+//     fullName: "jaredpalmer/formik",
+//     description: "Build forms in React, without the tears",
+//     language: "TypeScript",
+//     forksCount: 1589,
+//     stargazersCount: 21553,
+//     ratingAverage: 88,
+//     reviewCount: 4,
+//     ownerAvatarUrl: "https://avatars2.githubusercontent.com/u/4060187?v=4",
+//   },
+//   {
+//     id: "rails.rails",
+//     fullName: "rails/rails",
+//     description: "Ruby on Rails",
+//     language: "Ruby",
+//     forksCount: 18349,
+//     stargazersCount: 45377,
+//     ratingAverage: 100,
+//     reviewCount: 2,
+//     ownerAvatarUrl: "https://avatars1.githubusercontent.com/u/4223?v=4",
+//   },
+//   {
+//     id: "django.django",
+//     fullName: "django/django",
+//     description:
+//       "The Web framework for perfectionists with deadlines. Web framework for perfectionists with deadlines.",
+//     language: "Python",
+//     forksCount: 21015,
+//     stargazersCount: 48496,
+//     ratingAverage: 73,
+//     reviewCount: 5,
+//     ownerAvatarUrl: "https://avatars2.githubusercontent.com/u/27804?v=4",
+//   },
+//   {
+//     id: "reduxjs.redux",
+//     fullName: "reduxjs/redux",
+//     description: "Predictable state container for JavaScript apps",
+//     language: "TypeScript",
+//     forksCount: 13902,
+//     stargazersCount: 52869,
+//     ratingAverage: 0,
+//     reviewCount: 0,
+//     ownerAvatarUrl: "https://avatars3.githubusercontent.com/u/13142323?v=4",
+//   },
+// ];
